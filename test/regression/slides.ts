71af560555f7f93cab264b44d35a1f48ede9872c
chrisguttandin <chrisguttandin@media-codings.com>
2018-02-27 00:16:07 +0100

    feat: add visual regression tests

---
+++ test/regression/slides.ts
@@ -0,0 +1,233 @@
+import { browser, by } from 'protractor';
+import { elementShotMatchers, expectElementShot, resembleElementShot } from 'element-shot';
+
+const IS_SMOKE_TEST = !!process.env.IS_SMOKE_TEST;
+
+const navigateTo = (path: string) => {
+    return browser.get((IS_SMOKE_TEST) ? `https://chrisguttandin.github.io/web-audio-conference-2017/${ path }` : path);
+}
+
+describe('slides', () => {
+
+    beforeEach(() => {
+        jasmine.addMatchers(elementShotMatchers);
+
+        browser.driver
+            .manage()
+            .window()
+            .setSize(1024, 700);
+    });
+
+    describe('first slide', () => {
+
+        it('should look the same', async () => {
+            navigateTo('/');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-1');
+
+            expectElementShot(result).toBeRegressionFree();
+        });
+
+        it('should look the same', async () => {
+            navigateTo('/slides/1');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-1');
+
+            expectElementShot(result).toBeRegressionFree();// 0.2);
+        });
+
+    });
+
+    describe('second slide', () => {
+
+        it('should look the same', async () => {
+            navigateTo('/slides/2');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-2');
+
+            expectElementShot(result).toBeRegressionFree();
+        });
+
+    });
+
+    describe('third slide', () => {
+
+        it('should look the same', async () => {
+            navigateTo('/slides/3');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-3');
+
+            expectElementShot(result).toBeRegressionFree();
+        });
+
+    });
+
+    describe('fourth slide', () => {
+
+        it('should look the same', async () => {
+            navigateTo('/slides/4');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-4');
+
+            expectElementShot(result).toBeRegressionFree();
+        });
+
+    });
+
+    describe('fifth slide', () => {
+
+        it('should look the same', async () => {
+            navigateTo('/slides/5');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-5');
+
+            expectElementShot(result).toBeRegressionFree();// 0.6);
+        });
+
+    });
+
+    describe('sixth slide', () => {
+
+        it('should look the same', async () => {
+            navigateTo('/slides/6');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-6');
+
+            expectElementShot(result).toBeRegressionFree();// 1.1);
+        });
+
+    });
+
+    describe('seventh slide', () => {
+
+        it('should look the same', async () => {
+            navigateTo('/slides/7');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-7');
+
+            expectElementShot(result).toBeRegressionFree();
+        });
+
+    });
+
+    describe('eighth slide', () => {
+
+        it('should look the same', async () => {
+            navigateTo('/slides/8');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-8');
+
+            expectElementShot(result).toBeRegressionFree();// 0.7);
+        });
+
+    });
+
+    describe('nineth slide', () => {
+
+        it('should look the same', async () => {
+            navigateTo('/slides/9');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-9');
+
+            expectElementShot(result).toBeRegressionFree();// 0.7);
+        });
+
+    });
+
+    describe('tenth slide', () => {
+
+        it('should look the same', async () => {
+            navigateTo('/slides/10');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-10');
+
+            expectElementShot(result).toBeRegressionFree();// 0.6);
+        });
+
+    });
+
+    describe('eleventh slide', () => {
+
+        it('should look the same', async () => {
+            navigateTo('/slides/11');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-11');
+
+            expectElementShot(result).toBeRegressionFree();// 0.4);
+        });
+
+    });
+
+    describe('twelveth slide', () => {
+
+        it('should look the same', async () => {
+            navigateTo('/slides/12');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-12');
+
+            expectElementShot(result).toBeRegressionFree();// 0.8);
+        });
+
+    });
+
+    describe('thirteenth slide', () => {
+
+        it('should look the same', async () => {
+            navigateTo('/slides/13');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-13');
+
+            expectElementShot(result).toBeRegressionFree();
+        });
+
+    });
+
+    describe('fourteenth slide', () => {
+
+        it('should look the same', async () => {
+            navigateTo('/slides/14');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-14');
+
+            expectElementShot(result).toBeRegressionFree();
+        });
+
+    });
+
+    describe('fifteenth slide', () => {
+
+        it('should look the same', async () => {
+            navigateTo('/slides/15');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-15');
+
+            expectElementShot(result).toBeRegressionFree();
+        });
+
+    });
+
+    describe('sixteenth slide', () => {
+
+        it('should look the same', async () => {
+            navigateTo('/slides/16');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-16');
+
+            expectElementShot(result).toBeRegressionFree();
+        });
+
+    });
+
+    describe('seventeenth slide', () => {
+
+        it('should look the same', async () => {
+            navigateTo('/slides/17');
+
+            const result = await resembleElementShot(by.tagName('body'), 'test/screenshots/slide-17');
+
+            expectElementShot(result).toBeRegressionFree();
+        });
+
+    });
+
+});
