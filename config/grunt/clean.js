f792b8bf8ffcdf86238120d6492c345daf11c6b4
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-06-12 03:41:09 +0200

    build(config): handle files produced by differential loading

--- config/grunt/clean.js
+++ config/grunt/clean.js
@@ -1,6 +1,6 @@
 module.exports = {
     'runtime': [
-        'build/web-audio-conference-2017/runtime.*.js'
+        'build/web-audio-conference-2017/runtime-es*.js'
     ],
     'scripts': [
         'build/web-audio-conference-2017/!(ngsw-worker).js'
