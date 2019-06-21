54a17fa13ef4edadb4f5517f1b17506bf3853676
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-06-21 23:17:29 +0200

    build: fix chunk replacement

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -32,9 +32,9 @@ module.exports = {
         'copy:styles',
         'replace:styles',
         'clean:styles',
-        'htmlmin',
         'replace:chunks',
         'replace:csp-production',
+        'htmlmin',
         'replace:manifest',
         'gh-pages:deploy',
         'smoke'
