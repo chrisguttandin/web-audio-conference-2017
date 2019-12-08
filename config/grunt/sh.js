43aa6cf5bcc41c2e9c02ba103f43423246c8c3f6
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-12-08 19:22:28 +0100

    build(config): update grunt config

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -43,8 +43,8 @@ module.exports = (grunt) => {
         },
         'smoke': {
             cmd: (env.TRAVIS) ?
-                "IS_SMOKE_TEST=true ng e2e --dev-server-target '' && hint --tracking=off https://chrisguttandin.github.io/web-audio-conference-2017" :
-                "webdriver-manager update && IS_SMOKE_TEST=true ng e2e --dev-server-target '' --no-webdriver-update && hint --tracking=off https://chrisguttandin.github.io/web-audio-conference-2017"
+                "IS_SMOKE_TEST=true ng e2e --dev-server-target '' && hint --telemetry=off https://chrisguttandin.github.io/web-audio-conference-2017" :
+                "webdriver-manager update && IS_SMOKE_TEST=true ng e2e --dev-server-target '' --no-webdriver-update && hint --telemetry=off https://chrisguttandin.github.io/web-audio-conference-2017"
         },
         'test': {
             cmd: 'ng test --watch false'
