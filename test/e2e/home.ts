116dd06646c9e4de42ee395d0a051672f1b60573
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-11-24 03:49:48 +0100

    test: update e2e tests

--- test/e2e/home.ts
+++ test/e2e/home.ts
@@ -28,23 +28,23 @@ describe('/', () => {
         page = new HomePage();
     });
 
-    it('should display the correct headline', () => {
-        page.navigateTo();
+    it('should display the correct headline', async () => {
+        await page.navigateTo();
 
-        expect(page.getHeadline()).toEqual('Noiseless Web Audio tests');
+        expect(await page.getHeadline()).toEqual('Noiseless Web Audio tests');
     });
 
-    it('should go to the next slide', () => {
-        page.navigateTo();
+    it('should go to the next slide', async () => {
+        await page.navigateTo();
 
-        element(by.tagName('body')).sendKeys(Key.ARROW_RIGHT);
+        await element(by.tagName('body')).sendKeys(Key.ARROW_RIGHT);
 
         /*
          * @todo Unfortunately an arbitrary call browser.sleep() is used here as both element(by.tagName('body')).allowAnimations(false)
          * and browser.waitForAngular() have no effect.
          */
-        browser.sleep(1000);
+        await browser.sleep(1000);
 
-        expect(page.getSubHeadline()).toEqual('About me');
+        expect(await page.getSubHeadline()).toEqual('About me');
     });
 });
