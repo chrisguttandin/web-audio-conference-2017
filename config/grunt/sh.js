8a27d9868639d22d891eb3e6826338bf1b049d1f
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-04-19 09:58:46 +0200

    build(config): update grunt config

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -23,7 +23,7 @@ module.exports = (grunt) => {
             cmd: `htmlhint --config config/htmlhint/document.json 'src/**/index.html' && \
                 htmlhint --config config/htmlhint/template.json 'src/app/**/*.component.html' && \
                 npx ng lint web-audio-conference-2017 && \
-                npx stylelint src/**/*.css --config config/stylelint/config.json`
+                npx stylelint 'src/**/*.css' --config config/stylelint/config.json`
         },
         'lint-test': {
             cmd: 'npx ng lint web-audio-conference-2017 --configuration test'
