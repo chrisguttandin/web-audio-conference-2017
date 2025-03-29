2a6de14c0aa0f55314200fe9e39e2a6aacc2bd2f
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-03-29 01:03:17 +0100

    build(config): use npm scripts for testing tasks

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -7,23 +7,8 @@ module.exports = () => {
         'build': {
             cmd: 'npx ng build --base-href /web-audio-conference-2017/ --configuration production --subresource-integrity'
         },
-        'continuous': {
-            cmd: 'npx ng test'
-        },
-        'e2e': {
-            cmd: `npx playwright install --with-deps && \
-                npx playwright test --config config/playwright/config.ts`
-        },
         'rimraf-source-maps': {
             cmd: 'rimraf build/web-audio-conference-2017/browser/**.map'
-        },
-        'smoke': {
-            cmd: `npx playwright install --with-deps && \
-                IS_SMOKE_TEST=true npx playwright test --config config/playwright/config.ts && \
-                npx hint --telemetry=off https://chrisguttandin.github.io/web-audio-conference-2017`
-        },
-        'test': {
-            cmd: 'npx ng test --watch false'
         },
         'verify': {
             cmd: `npx bundle-buddy build/web-audio-conference-2017/browser/*.js.map && \
